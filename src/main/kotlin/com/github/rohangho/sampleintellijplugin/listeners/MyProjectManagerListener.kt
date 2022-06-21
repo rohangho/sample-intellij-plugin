96b96d00bebd70d8072c982a5667d9248cead8d7
GitHub Action <action@github.com>
2022-06-21 19:22:29 +0000

    Template cleanup

--- src/main/kotlin/com/github/rohangho/sampleintellijplugin/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/rohangho/sampleintellijplugin/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.rohangho.sampleintellijplugin.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.rohangho.sampleintellijplugin.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
---
+++ src/main/kotlin/com/github/rohangho/sampleintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.rohangho.sampleintellijplugin.services
+
+import com.github.rohangho.sampleintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
